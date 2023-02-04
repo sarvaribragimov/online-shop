2e4137ab4d7afac2ec5483ff000bc9f5d4f973b7
sarvaribragimov <sarvaribragimov980@gmail.com>
2023-02-04 09:04:52 +0500

    test

--- backend/src/apps/store/views.py
+++ backend/src/apps/store/views.py
@@ -1,33 +1,28 @@
 from .models.product import Product
 from django.views.generic.list import ListView
-
-# Create your views here.
-
-
-# class ProductListView(ListView):
-#     model = Product
-#     template_name = "store.html"
-#     context_object_name = "products"
-#     paginate_by = 10
-
-#     def get_queryset(self):
-#         return Product.objects.filter(is_available=True)
-
-
-# product_list_view = ProductListView.as_view()
-
 from django.shortcuts import render, get_object_or_404
 from .models.category import Category
+from django.core.paginator import Paginator, EmptyPage,\
+                                    PageNotAnInteger
+
 
 def product_list(request, category_slug=None):
+    min_price = request.GET.get("min")
+    max_price = request.GET.get("max")
+    if min_price and max_price:
+        products = Product.objects.filter(price__gte=min_price,price__lte=max_price)
+
     categories = Category.objects.all()
     products = Product.objects.all()
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
+    page = request.GET.get("page")
+    paginator = Paginator(products, 3)
+    products = paginator.get_page(page)  
     return render(request,
                   'store.html',
-                  {
+                  {"page":page,
                   'categories': categories,
                   'products': products})
 def product_detail(request, id, slug):
